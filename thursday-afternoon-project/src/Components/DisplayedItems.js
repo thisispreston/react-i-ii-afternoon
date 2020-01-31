7e2ff55fd2a6e0f6cca93ab415323ce5b46ba103
Preston Jenkins <prestonjenkins@Prestons-MacBook-Pro.local>
2020-01-30 22:26:57 -0700

    Yatta

--- thursday-afternoon-project/src/Components/DisplayedItems.js
+++ thursday-afternoon-project/src/Components/DisplayedItems.js
@@ -7,16 +7,18 @@ function DisplayedItems (props) {
       <div className="personName">
         {props.data.name.first} {props.data.name.last}
       </div>
+      <br />
       <div className="firstList">
         <li>From: {props.data.city}, {props.data.country}</li>
         <li>Job Title: {props.data.title}</li>
         <li>Employer: {props.data.employer}</li>
       </div>
+      <br />
       <div className="secondList">
         Favorite Movies:
-        <li>{props.data.favoriteMovies[0]}</li>
-        <li>{props.data.favoriteMovies[1]}</li>
-        <li>{props.data.favoriteMovies[2]}</li>
+        <li className="bulletList">{props.data.favoriteMovies[0]}</li>
+        <li className="bulletList">{props.data.favoriteMovies[1]}</li>
+        <li className="bulletList">{props.data.favoriteMovies[2]}</li>
       </div>
     </div>
   )
